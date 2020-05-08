e3d80d9c0336564a7cf1d508cf43ac9c42081733
Simonas Karuzas <simonas@not.cat>
2020-05-08 10:34:30 +0300

    fix: Credential card action buttons

--- web/src/components/CredentialCard.tsx
+++ web/src/components/CredentialCard.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Typography, CardHeader, CardMedia } from "@material-ui/core";
+import { Typography, CardHeader, CardMedia, Collapse, CardContent } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardActionAreaLink from "./Nav/CardActionAreaLink";
 import CardActions from "@material-ui/core/CardActions";
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import { makeStyles } from '@material-ui/core/styles';
-import ExposurePlus1Icon from '@material-ui/icons/ExposurePlus1';
+import RepeatIcon from '@material-ui/icons/Repeat';
 import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import clsx from 'clsx';
+
 import { formatDistanceToNow } from 'date-fns'
 import { Credential } from '../types'
 
@@ -70,11 +75,26 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0.8)'
-  }
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
 }));
 
 function CredentialCard(props: Props) {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   const { credential } = props
   return (
     <Card elevation={2}>
@@ -111,17 +131,57 @@ function CredentialCard(props: Props) {
             
             
       </CardActionAreaLink>
-      <CardActions className={classes.actions} >
+      <CardActions  disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+          <ThumbUpIcon />
         </IconButton>
         <IconButton aria-label="add to favorites">
-          <ExposurePlus1Icon />
+          <ThumbDownIcon />
+        </IconButton>
+        <IconButton aria-label="add to favorites">
+          <RepeatIcon />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
